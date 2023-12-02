13f153959d50882058c578706db8800cf43d2812
AliceLong <al6695@nyu.edu>
2023-12-02 17:12:26 -0500

    welcome fixed

--- front-end/src/Pages/Welcome.jsx
+++ front-end/src/Pages/Welcome.jsx
@@ -18,16 +18,8 @@ const Welcome = () => {
               Loading...
             </span>
           </div>
-          <div
-            className="inline-block h-12 w-12 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
-          </div>
         </div>
-        <p className="text-center">
+        <p className="text-center text-white font-title text-xl italic">
           Welcome to 31th Hotel, we are now processing your booking information
         </p>
       </div>
