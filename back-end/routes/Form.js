fc0e9c60d9a4059f421e914ec8b490c2800f18f4
AliceLong <al6695@nyu.edu>
2023-12-10 01:37:42 -0500

    form route finished

--- back-end/routes/Form.js
+++ back-end/routes/Form.js
@@ -1,24 +1,39 @@
-// routes/Form.js
-
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-// Define your route handlers
 router.get("/", (req, res) => {
-  // Handle GET request for /form
   res.send("Hello from the form route!");
 });
 
 router.post("/submit", (req, res) => {
-  // Handle POST request for /form/submit
-  // Access the form data from req.body
   const formData = req.body;
-  // Process the form data as needed
-  // ...
+  console.log(formData);
 
-  // Send a response
-  res.json({ message: "Form submitted successfully" });
+  // Create a new object with the form data, the user ID, and the submission time
+  const newUser = {
+    userId: uuidv4(),
+    name: formData.name,
+    phone: formData.phone,
+    email: formData.email,
+    reserveType: formData.reserveType,
+    submissionTime: new Date(), // Add this line
+  };
+
+  // Convert the newUser object to JSON
+  const json = JSON.stringify(newUser, null, 2);
+
+  // Write the JSON to userData.json
+  fs.writeFile("./data/userData.json", json, (err) => {
+    if (err) {
+      console.error("Error writing file:", err);
+      res.status(500).json({ message: "Error saving form data" });
+    } else {
+      console.log("Form data saved to userData.json");
+      res.json({ message: "Form submitted successfully" });
+    }
+  });
 });
 
-// Export the router
 module.exports = router;
